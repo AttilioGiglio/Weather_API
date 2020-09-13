24b6bce66fbc6bd2e50a6dc7d5c188d677b5acc6
attiliogiglio <attilio.g93@gmail.com>
2020-09-13 19:02:48 -0300

    adding form validation

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,28 +1,49 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 
 const Form = () => {
+    
     const [search, setSearch] = useState({
         ciudad: '',
         pais: ''
     })
+
+    const [error, setError] = useState(false)
+    
     // extraer ciudad y pais
-    const {ciudad,pais} = search;
+    const { ciudad, pais } = search;
+    
+    // funcion que coloca los elementos en el state
     const handleChange = (e) => {
         // actualizar el state
         setSearch({
-            ...search, [e.target.name] : e.target.value
+            ...search, [e.target.name]: e.target.value
         });
     }
+    // cuando el usuario da submit al formulario
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        // validar
+        if(ciudad.trim()===''||pais.trim()===''){
+            setError(true);
+            return;
+        }
+        setError(false);
+        // Pasar los cambios al component principal
+    }
+
     return (
-        <form className='input-field col s12'>
-            <input
-                type='text'
-                name='ciudad'
-                id='ciudad'
-                value={ciudad}
-                onChange={handleChange}
-            />
-            <label htmlFor='ciudad'>Ciudad:</label>
+        <form onSubmit={handleSubmit} >
+            {error ? <p className='red darken-4'>Todos los campos son obligatorios</p>:null}
+            <div className='input-field col s12'>
+                <input
+                    type='text'
+                    name='ciudad'
+                    id='ciudad'
+                    value={ciudad}
+                    onChange={handleChange}
+                />
+                <label htmlFor='ciudad'>Ciudad:</label>
+            </div>
             <div className='input-field col s12'>
                 <select name='pais' id='pais' value={pais} onChange={handleChange}>
                     <option value="US">Estados Unidos</option>
@@ -33,6 +54,9 @@ const Form = () => {
                     <option value="ES">España</option>
                     <option value="PE">Perú</option>
                 </select>
+            </div>
+            <div className='input-field col s12'>
+                <input type='submit' value='Buscar Clima' className='waves-effect waves-light btn-large btn-block yellow accent-4' />
             </div>
         </form>
     )
